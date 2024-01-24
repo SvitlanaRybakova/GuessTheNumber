54632b04e529e2cca731500b3ba984795c0e7138
Svitlana Rybakova <rsv0011@gmail.com>
2024-01-24 20:11:12 +0100

    Implement a simple number guessing game  with limited attempts and random number generation

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
+            int number;
+            int minValue, maxValue;
+            int triesCount = 5;
+            int userInput;
+            Random rand = new Random();
+
+            number = rand.Next(0, 101);
+            minValue = rand.Next(number - 10, number);
+            maxValue = rand.Next(number + 1, number + 10);
+
+            Console.WriteLine($"Try to determine the number. It surpasses {minValue} but lower than {maxValue}");
+            Console.WriteLine($"What is the number? You have {triesCount} attempts");
+
+            while (triesCount-- > 0)
+            {
+                Console.WriteLine("Type your answer: ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+
+                if(userInput == number)
+                {
+                    Console.WriteLine( $"Congrats! This is a right number. The number was {number}.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, this number is incorrect. Try more");
+                }
+            }
         }
     }
 }
